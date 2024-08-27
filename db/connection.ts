512078d10052d8cee261cfcb08afb163b942e467
SADAF MTAHERI <sadaf@SADAFs-MacBook-Pro.local>
2024-08-27 15:15:34 +0100

    Update project structure and configuration for production

--- db/connection.ts
+++ db/connection.ts
@@ -59,18 +59,15 @@ dotenv.config();
 
 const ENV = process.env.NODE_ENV || "development";
 
-// Check for required environment variables
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-// Configuration for the PostgreSQL connection pool
 const config: any = {};
 
-// Set up the connection pool based on the environment
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2; // Limits the pool size to 2 connections for free tier limits
+  config.max = 2; // Adjust the max connections according to your database constraints
 } else {
   config.user = process.env.PGUSER;
   config.host = process.env.PGHOST;
@@ -79,7 +76,6 @@ if (ENV === "production") {
   config.port = parseInt(process.env.PGPORT || "5432", 10);
 }
 
-// Create a new pool instance with the configuration
 const pool = new Pool(config);
 
 export default pool;
